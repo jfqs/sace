065f84b77fe739084656f7cb6f3f00f24a6a8795
Javier <jquarites@gmail.com>
2016-06-01 02:58:11 -0500

    added pundit configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   #after_action :verify_authorized, unless: :devise_controller?
+
+  after_action :verify_authorized, unless: :devise_controller?
+
 end
